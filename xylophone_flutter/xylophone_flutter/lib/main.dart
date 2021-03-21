597e1168d67c33c9dd53c14f53da9e709e368460
itcanbecoded <doherty.steve.j@gmail.com>
2021-03-21 12:17:30 -0500

    update packages to include english words

--- xylophone_flutter/xylophone_flutter/lib/main.dart
+++ xylophone_flutter/xylophone_flutter/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
@@ -10,8 +9,9 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Text(nouns.first),
         ),
       ),
     );
   }
+}
