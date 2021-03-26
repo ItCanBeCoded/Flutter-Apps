55593ad9300d5b24341217690637e98367d2fd76
itcanbecoded <doherty.steve.j@gmail.com>
2021-03-26 15:32:42 -0500

    added networking class and update UI with live weather data api

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,4 +1,11 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
+import '../services/location.dart';
+import '../services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = 'a2769c5621107683fb339a76a7eb16b5';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,15 +13,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+    print('this line of code is triggered');
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(locationWeather: weatherData);
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitHourGlass(
+          color: Colors.blue.shade600,
+          size: 50.0,
         ),
       ),
     );
