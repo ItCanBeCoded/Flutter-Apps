d22dc275addc39a6467365586b02371a0caee0c7
itcanbecoded <doherty.steve.j@gmail.com>
2021-03-26 17:30:06 -0500

    add search functionality to get weather from any city

--- clima/README.md
+++ clima/README.md
@@ -1,1 +1,4 @@
-basic weather app with live data api
+##basic weather app with live data api
+
+##you will need to make a profile and use your weather api key from
+## openweathermap.org
--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,11 +1,8 @@
 import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
-import '../services/location.dart';
-import '../services/networking.dart';
 import 'location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = 'a2769c5621107683fb339a76a7eb16b5';
+import '../services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,31 +10,19 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double latitude;
-  double longitude;
-
   @override
   void initState() {
     super.initState();
     getLocationData();
-    print('this line of code is triggered');
   }
 
   void getLocationData() async {
-    Location location = Location();
-
-    await location.getCurrentLocation();
-
-    latitude = location.latitude;
-    longitude = location.longitude;
-
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen(locationWeather: weatherData);
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
     }));
   }
 
